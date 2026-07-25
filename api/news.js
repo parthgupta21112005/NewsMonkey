ecfecbdec34893c18804a042482ab12b431ab338
Parth <parthgupta21112005@gmail.com>
2026-07-25 13:40:05 +0530

    deploy 4

--- api/news.js
+++ api/news.js
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
       pageSize = 5,
     } = req.query;
     
+    const apiKey = process.env.NEWS_API_KEY;
     if (!apiKey) {
       return res.status(500).json({
         status: "error",
